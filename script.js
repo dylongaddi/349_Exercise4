4f536d67dba6004e0e60e926fbbdfe6fc050c335
dylongaddi <dylongaddi@gmail.com>
2023-10-20 22:04:34 -0700

    added async details functionality

--- script.js
+++ script.js
@@ -28,35 +28,73 @@ function getArticleId(comments) {
   return data.postId;
 }
 
-const details = document.getElementsByTagName("details");
-for (const detail of details) {
-  detail.addEventListener("toggle", async (event) => {
-    if (detail.open) {
-      const asides = detail.getElementsByTagName("aside");
-      const commentsWereDownloaded = asides.length > 0;
-      if (!commentsWereDownloaded) {
-        const articleId = getArticleId(detail);
-        const comments = await downloadComments(articleId);
-        console.log(comments);
-      }
-    }
-  });
+function capitalizeFirstLetter(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
-const posts = await downloadPosts();
+const posts = await downloadPosts(2);
 console.log(posts);
 
 
-posts.forEach(post => {
-  console.log(post.userId)
-  let postBody = post.body.replaceAll(/\n/g, "<br>");
-  articleContainer.innerHTML += 
-  `<article data-post-id=${post.id}>
-    <h2>${post.title}</h2>
-    <aside>
-      <span>${getUserName(post.userId)}</span>
-    </aside>
-      ${postBody}
-   </article>`
+posts.forEach(async post =>  { //template string literal for each post
+  const userName = await getUserName(post.userId)
+    .catch(error => {
+      console.error("Error occurred")
+      console.log(error)
+    })
+  const postBody = capitalizeFirstLetter(post.body.replaceAll(/\n/g, "<br>"));
+  const postContent = `<article data-post-id=${post.id}>
+  <h2>${post.title}</h2>
+  <aside>
+    <span>${userName}</span>
+  </aside>
+    ${postBody}
+ </article>`
+ 
+  const detailSection = 
+  `
+  <details class="details">
+  <summary>See what our readers had to say...</summary>
+    <section class="sections">
+      <header>
+        <h3>
+          Comments
+        </h3>
+      </header>
+    </section>
+ </details>
+ `
+  articleContainer.insertAdjacentHTML("beforeend", postContent)
+  articleContainer.insertAdjacentHTML("beforeend", detailSection)
+  
+  const detail = articleContainer.querySelector(".details:last-of-type"); //get last of type so we dont have to iterate over every detail each time in a forEach
+  detail.addEventListener("toggle", async (event) => {
+    if (detail.open) {
+      const asides = detail.getElementsByTagName("aside")
+      const commentsWereDownloaded = asides.length > 0;
+      if (!commentsWereDownloaded) {
+        const articleId = getArticleId(detail)
+        const comments = await downloadComments(articleId);
+        const section = detail.querySelector("section")
+        comments.forEach(comment => { //template string literal for each comment
+          const commentBody = capitalizeFirstLetter(comment.body.replaceAll(/\n/g, "<br>"));
+          const commentName = capitalizeFirstLetter(comment.name)
+          const commentSection = 
+          `<aside>
+            ${commentBody}
+            <br></br>
+            <small>${commentName}</small>
+           </aside>
+          `
+          section.insertAdjacentHTML("beforeend", commentSection)
+        })
+      }
+    }
+})
 });
+
+
+
+
+
 
